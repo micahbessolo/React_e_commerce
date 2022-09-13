70778c2ed709ad09e9ee14b520956db373dee1b0
Micah Bessolo <83624003+micahbessolo@users.noreply.github.com>
2022-09-12 20:14:05 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
+import { Products, Navbar, Cart, Checkout } from './components';
 
-// import Products from './components/Products/Products';
-// import Navbar from './components/Navbar/Navbar';
+const App = function() {
+    // const current_cart = commerce.cart;
+    // console.log(current_cart);
 
-import { Products, Navbar } from './components';
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
-const App = () => {
-  return (
-    <div>
-        <Navbar />
-        <Products />
-    </div>
-  )
-}
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    };
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    };
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+        console.log('this works')
+    };
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+
+
+    function defined(thing) {
+        if (Object.keys(thing).length === 0) {
+            return "empty"
+            // console.log(thing)
+        }
+        else return thing
+    }
+  
+
+    let numba = defined(cart);
+    // console.log(defined(cart.total_items))
+    console.log(numba)
+    // console.log(cart)
+
+    function notFalse(thingy) {
+
+    }
+
+    return (
+        <div>
+            <Navbar totalItems={cart.total_items} />
+            <Products products={products} onAddToCart={handleAddToCart} />
+        </div>
+    );
+};
 
 export default App
--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,3 +1,3 @@
-import Commerce from '@ahec/commerce.js';
+import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce();
+export const commerce = new Commerce('pk_468306db5ca040bdc41948fd40d4989b57e300c68c254', true);
